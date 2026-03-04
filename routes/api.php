9e625a69a7766a596e6994bad474c6eb13d30813
SiJibrilll <jibrilianwa@gmail.com>
2026-03-04 19:53:30 +0700

    added read categories

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,13 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ItemController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('items', ItemController::class);
+
+Route::apiResource('categories', CategoryController::class);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
